36cc49f088366b837244f832c6ed2b6fd9f10b1f
saraivafelipe27 <saraivafelipe27@gmail.com>
2020-09-08 15:09:30 -0300

    PROMISES

--- aula.js
+++ aula.js
@@ -722,17 +722,47 @@
 
 
 
+// const xhr = new XMLHttpRequest();
+
+// xhr.open('GET', 'https://api.github.com/users/saraivafelipe27');
+// xhr.send(null);
+
+
+// xhr.onreadystatechange = function() {
+//    if(xhr.readyState === 4) {
+//       console.log(JSON.parse(xhr.responseText));
+//    }
+// }
 
 
 
-const xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://api.github.com/users/saraivafelipe27');
-xhr.send(null);
+// // PROMISES
 
 
-xhr.onreadystatechange = function() {
-   if(xhr.readyState === 4) {
-      console.log(JSON.parse(xhr.responseText));
-   }
+var minhaPromise = function() {
+   return new Promise(function(resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', 'https://api.github.com/users/saraivafelipe27');
+      xhr.send(null);
+
+      xhr.onreadystatechange = function() {
+         if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+               resolve(JSON.parse(xhr.responseText));
+            } else {
+               reject('Erro na requisição');
+            }
+         }
+      }
+   })
 }
+
+
+minhaPromise()
+   .then(function(response){
+      console.log(response);
+   })
+   .catch(function(error){
+      console.warn(error);
+   });
