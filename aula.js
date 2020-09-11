1085ebb40fff80af4718acca03ec9f3f82cb7445
saraivafelipe27 <saraivafelipe27@gmail.com>
2020-09-10 22:24:52 -0300

    update code

--- aula.js
+++ aula.js
@@ -707,95 +707,11 @@
 // }
 
 
-// // requisitions AJAX 
-
-// const xhr = new XMLHttpRequest();
-
-// xhr.open('GET', 'https://api.github.com/users/saraivafelipe27');
-// xhr.send(null);
-
-// xhr.onreadystatechange = function() {
-//    if (xhr.readyState === 4) {
-//       console.log(JSON.parse(xhr.responseText));
-//    }
-// }
-
-
-
-// const xhr = new XMLHttpRequest();
-
-// xhr.open('GET', 'https://api.github.com/users/saraivafelipe27');
-// xhr.send(null);
-
-
-// xhr.onreadystatechange = function() {
-//    if(xhr.readyState === 4) {
-//       console.log(JSON.parse(xhr.responseText));
-//    }
-// }
-
-
-
-
-// // PROMISES 
-
-
-// const myPromise = () => new Promise((resolve, reject) => {
-//    setTimeout(() => { resolve('T E S T E >>>') }, 3000);
-// })
-
-
-// myPromise().then(response => {
-//    console.log(response);
-// })
 
 
 
 
 
 
-// var minhaPromise = function() {
-//    return new Promise(function(resolve, reject) {
-//       var xhr = new XMLHttpRequest();
-//       xhr.open('GET', 'https://api.github.com/users/saraivafelipe27');
-//       xhr.send(null);
-
-//       xhr.onreadystatechange = function() {
-//          if (xhr.readyState === 4) {
-//             if (xhr.status === 200) {
-//                resolve(JSON.parse(xhr.responseText));
-//             } else {
-//                reject('Erro na requisição');
-//             }
-//          }
-//       }
-//    })
-// }
-
-
-// minhaPromise()
-//    .then(function(response){
-//       console.log(response);
-//    })
-//    .catch(function(error){
-//       console.warn(error);
-//    });
-
-
-// // Utilizando AXIOS
-
-// axios.get('https://api.github.com/users/saraivafelipe')
-//    .then(function(response){
-//       console.log(response);
-//    })
-//    .catch(function(error){
-//       console.warn(error);
-//    });
-
-
-
-
-
-// // Async Await  
 
 
